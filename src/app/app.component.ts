f6e41093cd7c18814f35e0f4b63ee4910e9e77a0
LavanyaBS2022 <lavanyabs2022@gmail.com>
2022-12-06 17:12:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup , FormControl , Validators, FormArray,FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
+  reactiveForm:FormGroup;
+//education: any;
+  //user: User
+
+  constructor() {
+    this.reactiveForm=new FormGroup({
+      firstName : new FormControl(null,[Validators.required , this.noSpace]),
+      lastName : new FormControl(null,Validators.required),
+      email : new FormControl(null,[Validators.required,Validators.email]),
+      country : new FormControl(null,Validators.required),
+      gender: new FormControl(null,Validators.required), 
+      education: new FormArray([
+        new FormControl(''),
+      ])
+        
+    });
+  }
+  onSubmitButtonClick(){
+    console.log(this.reactiveForm)
+  }
+
+  noSpace(control:FormControl){
+    if(control.value!= null && control.value.indexOf(' ')!=-1){
+      return{ noSpace: true}
+    }
+    return null;
+  }
+
+  reset(){
+    this.reactiveForm.reset();
+  }
+
+  get education() {
+    return this.reactiveForm.get('education')as FormArray;
+  }
+ 
+  add() {
+    this.education.push(new FormControl(''));
+ }
+
 }
